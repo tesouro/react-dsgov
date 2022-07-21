e42dc64c1bf5f5b7c797a719d34023b56a769a0d
Igor Augusto <igorgutofc@gmail.com>
2022-07-21 13:01:03 -0300

    Documentação do componente Switch.

--- src/components/Switch/Switch.tsx
+++ src/components/Switch/Switch.tsx
@@ -3,21 +3,38 @@ import React from "react";
 import IMtProps from "../IMtProps";
 import { useSpreadProps } from "../Util/useSpreadProps";
 import { useMtProps } from "../Util/useMtProps";
+import uniqueId from 'lodash.uniqueid';
+
 
 interface SwitchProps  extends React.HTMLAttributes<HTMLDivElement>, IMtProps {
+    /** Label do Switch. */
     label: string
+    /** Densidade do switch.
+     * 
+     * - small: pequena
+     * - medium: média
+     * - large: grande
+     */
     density?: "small" | "medium" | "large"
+    /** Texto para o switch com valor true. */
     enabledText?: string,
+    /** Texto para o switch com valor false. */
     disabledText?: string,
+    /** Mostra ícone dentro do switch */
     showIcon?: boolean
+    /** Posição da label.
+     * 
+     * - left: à esquerda do switch.
+     * - top: acima do switch.
+     * - right: à direita do switch.
+     */
     labelPosition?: "left" | "top" | "right"
 } 
 
 const Switch = React.forwardRef<HTMLDivElement, SwitchProps>(
-    ({className, children, id, label, density = "medium", enabledText, disabledText, showIcon = false, labelPosition = "left", ...props}, ref) => {
+    ({className, children, id = uniqueId("switch_____"), label, density = "medium", enabledText, disabledText, showIcon = false, labelPosition = "left", ...props}, ref) => {
         const mtProps = useMtProps(props);
         const spreadProps = useSpreadProps(props);
-
 
         return (
             <div
