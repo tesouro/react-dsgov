d14be6766c65faeebbe5963a30c66404f2d423d3
Igor Augusto <igorgutofc@gmail.com>
2022-07-21 11:38:54 -0300

    Documentação do MagicButton.

--- src/components/MagicButton/MagicButton.tsx
+++ src/components/MagicButton/MagicButton.tsx
@@ -7,10 +7,21 @@ import Button from "../Button";
 
 
 interface MagicButtonProps  extends React.HTMLAttributes<HTMLButtonElement>, IMtProps {
-} 
+        /** Densidade do magic button.
+         * 
+         * - large: alta
+         * - medium: média
+         * - small: pequena
+         */
+        density: "large" | "small" | "medium";
+        /** Se o botão é circular. */
+        circle?: boolean,
+        /** Classe de ícone FontAwesome para o botão. */
+        icon?: string
+}
 
 const MagicButton = React.forwardRef<HTMLButtonElement, MagicButtonProps>(
-    ({className, children, ...props}, ref) => {
+    ({className, children, density = "medium", ...props}, ref) => {
         const mtProps = useMtProps(props);
         const spreadProps = useSpreadProps(props);
         const refWrapper = useRef(null);
@@ -26,6 +37,7 @@ const MagicButton = React.forwardRef<HTMLButtonElement, MagicButtonProps>(
                 ref={refWrapper}
                 className={classNames(
                     "br-magic-button",
+                    density,
                     className,
                     ...mtProps
                 )}                
---
+++ src/components/MagicButton/Readme.md
@@ -0,0 +1,24 @@
+O componente Magic Button é um botão com grande apelo visual.
+
+# Texto
+```js
+    <MagicButton>Label</MagicButton>
+```
+
+# Ícone
+```js
+    <MagicButton circle icon="fas fa-cart-plus" />
+```
+
+# Densidade baixa
+```js
+    <MagicButton density="small">Label</MagicButton>
+```
+# Densidade média
+```js
+    <MagicButton density="medium">Label</MagicButton>
+```
+# Densidade alta
+```js
+    <MagicButton density="large">Label</MagicButton>
+```
